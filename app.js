864ca6674483d1777b1f2ab3458e66e58f4d9eab
Dakota Lambert <dakotalambert117@gmail.com>
2021-06-22 20:12:02 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+var $;
+$(document).ready(function () {
+    console.log("ready function called.");
+
+    //Button Click on Enter
+    $("#countryCodeTextInput").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#LookupButton").click();
+        }
+    });
+    //Button Click calls callWebService() Function
+    $("#LookupButton").on("click", callWebService);
+});
+
+function callWebService() {
+    console.log("Certified")
+}
