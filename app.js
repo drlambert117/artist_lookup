3fbc85791bec946cddbafce6a5a0f2f3b90f0148
Dakota Lambert <dakotalambert117@gmail.com>
2021-06-22 20:31:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-var $;
-
-    //Button Click on Enter
-    $("#countryCodeTextInput").keyup(function (event) {
-        if (event.keyCode == 13) {
-            $("#LookupButton").click();
-        }
-    });
-    //Button Click calls callWebService() Function
-    $("#LookupButton").on("click", callWebService);
-});
-
-function callWebService() {
-    console.log("Certified")
-}
+function Lookup() {console.log("Certified")};
